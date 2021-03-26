c43e166471a533f7d74d6549fa315084b139109f
Brian <brian@Brian.HP.Laptop>
2021-03-26 16:07:44 -0700

    restyled mods

--- src/components/ClickMod.js
+++ src/components/ClickMod.js
@@ -13,17 +13,13 @@ const ClickMod = (props) =>{
 
     return(
         <motion.div onClick={(e)=>e.stopPropagation()} initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}  transition={{duration:.3}} className="click-mod-wrapper">
-            <svg className="back-svg" viewBox="0 0 218 661" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M19 0V158.5L109 248.5V356L199 446V661" stroke="rgba(0,0,0,.2)" strokeWidth="5"/>
-                <path d="M109 0V159.5L199 249.5V357L109 447V661" stroke="rgba(0,0,0,.2)" strokeWidth="5"/>
-                <path d="M199 0V160.5L19 340.5V661" stroke="rgba(0,0,0,.2)" strokeWidth="5"/>
-            </svg>
 
             <div className="exit" onClick={props.exit}>
                 x
             </div>
             
             <div className="title-wrap">
+                <h1 className="num" >01</h1>
                 <h1>{project?.name}</h1>
             </div>
             <div className="content-wrap">
