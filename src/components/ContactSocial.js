4823336088022808448b4465fc9bd21582c5fe8c
brianadams731 <brianadams731@gmail.com>
2021-03-28 11:09:40 -0700

    Added Email to connect

--- src/components/ContactSocial.js
+++ src/components/ContactSocial.js
@@ -29,6 +29,13 @@ const ContactSocial = () =>{
                     <h3>Linkedin</h3>
                 </div>
             </div>
+            <div className="email-container">
+                <svg fill="white" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 330.001 330.001">
+	                <path id="XMLID_350_" d="M173.871,177.097c-2.641,1.936-5.756,2.903-8.87,2.903c-3.116,0-6.23-0.967-8.871-2.903L30,84.602L0.001,62.603L0,275.001c0.001,8.284,6.716,15,15,15L315.001,290c8.285,0,15-6.716,15-14.999V62.602l-30.001,22L173.871,177.097z"/>
+	                <polygon id="XMLID_351_" points="165.001,146.4 310.087,40.001 19.911,40"/>
+                </svg>
+                <h3>brianadams731@gmail.com</h3>
+            </div>
         </div>
     )
 }
