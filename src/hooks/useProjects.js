836269dc6538dd5c19b627f8a779966f3b54a758
brianadams731 <brianadams731@gmail.com>
2021-04-08 22:59:28 -0700

    Added Images and Descriptions

--- src/hooks/useProjects.js
+++ src/hooks/useProjects.js
@@ -6,21 +6,21 @@ const useProjects = (project,name) =>{
             recypher:{
                 name:"Recypher",
                 img: [],
-                desc:"Translates English text into French",
+                desc:"This is a chrome extension built to aid language learning. I am an active reader of various newsletters and news sites like medium and programming digest, and also learning french. This extension allows me to do both at the same time. This extension searches the pages you visit prioritising specific symantic html tags, randomly selects sentences then translates those sentences into french, and injects them back into the page. Upon hovering translated text the extension will show the original sentence in english. Secondly when reading full french text like articles found on Le Monde a user can highlight any text, press a control + shift + 3 and get an english translation of the highlighted selection displayed on the page. This 2 stage approach helps involve the learner at every step in the learning journey and makes the french language as asseccable as possible. The chrome extension is build using JS, HTML, and CSS. While the backend is build using Flask and Azure.",
                 tech: ["js","python","flask","html","css"],
                 link: null,
             },
             sideScroller:{
                 name:"Side Scroller",
                 img: ["cartrage.png","Death - Copy.png"],  // loading from public
-                desc: "Inspired by retro games like Castlvenia, made using pure js Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                desc: "Inspired by retro games like Castlevenia and Super Mario Bros, made using JavaScript (ES6+) and HTML canvas. I decided to not use any external libraries building this project, instead using this as an opportunity to solve problems like tile maps, enemy logic and looping parallax with my own implementation. This project was a great opportunity to focus on my javascript fundimentals and maintain project structure.",
                 tech: ["js", "html","css"],
                 link: "https://brianadams731.github.io/sideScrollerResume/sideScroller/",
             },
             elderLawForm:{
                 name:"Elder Law Form",
-                img: [],
-                desc:"Large form application used to onboard new clients",
+                img: ["elderform1.jpg","elderform2.jpg","elderform3.jpg"],
+                desc:"Application used to onboard new clients at a local law office. This is a complex form featuring 7 pages of various form elements, including a 2 dynamic fields that expand to add more fields if needed. This application is animated with Framer Motion, which gives the progress bar and card animations a more polished and natural feel. This application is using  React Hook Forms which helped with form validation as well as speed up the development process. The onboarding application uses a Flask backend which parses the data collected into a shape the client desired, as well as updating the client when a potential client submits the form, then sends the data to the attorney.",
                 tech: ["js","react","html","css"],
                 link: null,
             },
