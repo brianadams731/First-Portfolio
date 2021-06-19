a7a584a3f75d34b7e826a3107e3291c3b7bfb06f
brianadams731 <brianadams731@gmail.com>
2021-06-19 14:28:03 -0700

    Fixed contact position bug

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ import "../styles/contact.css"
 
 const Contact = (props) =>{
     return (
-        <motion.div onClick={(e)=>{e.stopPropagation()}} initial={{x:"-100vw"}} animate={{x:0}} exit={{x:"100vw"}} transition={{duration:.6}} className="contact-wrap">
+        <motion.div onClick={(e)=>{e.stopPropagation()}} initial={{x:"-150vw", y:"-50%"}} animate={{x:"-50%", y:"-50%"}} exit={{x:"75vw", y:"-50%"}} transition={{duration:.6}} className="contact-wrap">
             {props.typeKey==="form"?<ContactForm exit={props.exit}/>:<ContactSocial />}
         </motion.div>
     )
