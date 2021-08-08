7e09502b55ac811dcb45497c1aceea2ea81017b1
Brian <brianadams731@gmail.com>
2021-08-07 17:25:08 -0700

    Fixed Copy

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -8,7 +8,7 @@ const Interests = () =>{
                 <h1>Hello</h1>
                 <div className="under-line"></div>
             </div>
-            <p className="intro">My name is Brian and I am a web developer who is currently finishing my last year at San Francisco State University, majoring in Computer Science.</p>
+            <p className="intro">My name is Brian, I am a web developer and am currently finishing my last year at San Francisco State University, majoring in Computer Science.</p>
             <p className="other">I am a driven web developer, who has the desire to learn new and existing technologies while also developing my programming skills. Please feel free to navigate and explore my varous projects.</p>
         </div>
     )
